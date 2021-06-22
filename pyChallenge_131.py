e45f6ea215a2218036e8548885e96362bd63717b
DasisCore <84772914+DasisCore@users.noreply.github.com>
2021-06-22 16:54:10 +0900

    Update pyChallenge_131.py
    카페에서 재 작성해서 업로드

--- pyChallenge_131.py
+++ pyChallenge_131.py
@@ -1,63 +1,36 @@
 from tkinter import *
 import csv
-
 window = Tk()
-window.title("Personal information")
-window.geometry("400x310")
-#def part-----------------------------------------------------------------------
-def add():
-    nam = name.get()
-    ag = age.get()
-    plus = str(nam), ",", int(ag)
-    pi_list.insert(END, plus)
-    name.delete(0, END)
-    age.delete(0, END)
-    name.focus()
+window.title("Age")
+window.geometry("300x100")
 
-def delete():
-    pi_list.delete(0)
-    name.delete(0, END)
-    age.delete(0, END)
-    name.focus()
-
-def save():  #여기 수정해야 함!
-    file = open("Personal information.csv" , "w")
-    tmp = []
-    tmp = pi_list.get(0, END)
-    for i in tmp:
-        record = str(i) + "\n"
-        file.write(str(record))
-    file.close()
-
-#-------------------------------------------------------------------------------
-label = Label(text = "Please enter your name and age. ")
-label.place(x = 30, y = 30)
+def click():
+    file = open("Personal information.csv", "a")
+    name = namebox.get()
+    age = agebox.get()
+    record = name + ',' + age + "\n"
+    file.write(str(record))
+    namebox.delete(0, END)
+    agebox.delete(0, END)
+    namebox.focus()
 
 label1 = Label(text = "Name")
-label1.place(x = 30, y = 60)
+label1.place(x = 30, y = 20)
 
-name = Entry(text = "")
-name.place(x = 70, y = 60, width = 90, height = 22)
-name["justify"] = "center"
-name.focus()
+namebox = Entry()
+namebox.place(x = 30, y = 40, width = 100, height = 25)
+namebox["justify"] = "center"
+namebox.focus()
 
 label2 = Label(text = "Age")
-label2.place(x = 170, y = 60)
+label2.place(x = 160, y = 20)
 
-age = Entry(text = "")
-age.place(x = 200, y = 60, width = 30, height = 22)
-age["justify"] = "center"
+agebox = Entry()
+agebox.place(x = 160, y = 40, width = 50, height = 25)
+agebox["justify"] = "center"
 
-add_button = Button(text = "Add", command = add)
-add_button.place(x = 250, y = 30, width = 50, height = 22)
+button = Button(text = "Save", command = click)
+button.place(x = 220, y = 40, width = 50, height = 25)
 
-del_button = Button(text = "Delete", command = delete)
-del_button.place(x = 310, y = 30, width = 50, height = 22)
-
-csv_button = Button(text = "Save the csv file", command = save)
-csv_button.place(x = 250, y = 60, width = 110, height = 22)
-
-pi_list = Listbox()
-pi_list.place(x = 30, y = 90, width = 330, height = 200)
 
 window.mainloop()
